eeb6b5eda3bbbc9cfc499f25925737f744d4f400
Tasleemofx <oladepotesleem5@gmail.com>
2022-09-17 04:21:06 +0100

    additional styling

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -15,12 +15,14 @@ const Showcase=()=>{
                 Keep track of products and process orders
             </h3>
             <img src="./images/high.png" alt="background" />
-            <Card card="card1" item={<FaRegMoneyBillAlt/>} 
-            para={"Emerald investment requested 3 bags of rice"}/>
-            <Card card="card2" item={ <FaFeather/> }
-            para={"You processed 21bags of beans today"}/>
-            <Card card="card3" item={<FaHandHoldingMedical/>}
-            para={"You have recieved a payment from mavi ltd"}/>
+            <div className="show-grid">
+                <Card card="card1" item={<FaRegMoneyBillAlt/>} 
+                para={"Emerald investment requested 3 bags of rice"}/>
+                <Card card="card2" item={ <FaFeather/> }
+                para={"You processed 21bags of beans today"}/>
+                <Card card="card3" item={<FaHandHoldingMedical/>}
+                para={"You have recieved a payment from mavi ltd"}/>
+            </div>
             
         </div>
     )
